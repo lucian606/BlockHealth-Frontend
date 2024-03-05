99741a29ea61331655848a40947c4ef1961be873
lucian606 <iliesculucian1508@gmail.coom>
2024-03-05 22:22:13 +0200

    Change utils file to TS

--- src/utils.js
+++
@@ -1,4 +0,0 @@
-export const backendUrl = "http://localhost:5000";
-
-export const registerUrl = `${backendUrl}/api/auth/register`;
-export const loginUrl = `${backendUrl}/api/auth/login`;
---
+++ src/utils.ts
@@ -0,0 +1,4 @@
+export const backendUrl:string = "http://localhost:5000";
+
+export const registerUrl:string = `${backendUrl}/api/auth/register`;
+export const loginUrl:string = `${backendUrl}/api/auth/login`;
