27982bfd5ff9ff2f358e1f81db660207137776b9
lucian606 <iliesculucian1508@gmail.coom>
2024-03-09 02:48:30 +0200

    Stylize Welcom Patient page

--- src/pages/patientPages/PatientMainPage.tsx
+++ src/pages/patientPages/PatientMainPage.tsx
@@ -7,7 +7,7 @@ export default function PatientMainPage() {
 
 	const pages = [
 		{
-			name: "Home",
+			name: "Welcome",
 			route: "/",
 		},
 		{
@@ -36,11 +36,17 @@ export default function PatientMainPage() {
 	return (
 		<div className="flex flex-col h-screen">
 			<Navbar pages={pages} currentPage="Home" />
-			<div className="flex-grow bg-gray-800 overflow-auto text-white">
-				<h1>Main Page</h1>
-				<p>
-					{" "}
-					You are logged in as: {user?.name} {user?.role}{" "}
+			<div className="p-5 flex flex-col flex-grow bg-gray-800 overflow-auto text-white items-center justify-center">
+				<img className="w-40 h-40" src="/care.svg" alt="logo" />
+				<p className="text-2xl md:text-4xl font-bold">
+					Hello {user?.name}👋
+				</p>
+				<p className="text-2xl md:text-4xl font-bold pb-1">
+					Welcome to BlockHealth!
+				</p>
+				<p className="text-sm md:text-md">
+					Below you can find more details about how to use our
+					platform
 				</p>
 			</div>
 		</div>
